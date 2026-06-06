13b93c1b3d881edbef168d566cd0ced9c7c5e7ef
adfghjklz <xuannhuan9@gmail.com>
2026-06-06 16:58:02 +0800

    Update omni_holi.mk

--- omni_holi.mk
+++ omni_holi.mk
@@ -10,7 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, 
+$(SRC_TARGET_DIR)/product/core_64_bit.mk
+$(SRC_TARGET_DIR)/product/base.mk
 
 # Inherit from holi device
 $(call inherit-product, device/qualcomm/holi/device.mk)
